280c575b923ec28c899c2e094cd915f5a56b8342
NisthaGoyal <68862929+NisthaGoyal@users.noreply.github.com>
2021-07-03 09:11:46 +0530

    Add files via upload

---
+++ binary search.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+void binary_search();
+int a[50],n,item,loc,beg,mid,end,i;
+  void main()
+{
+
+printf("\nEnter  the size of an array :");
+scanf("%d",&n);
+printf("\n Enter element of an array in sorted form:\n");
+for(i=0; i<n;i++)
+scanf("%d",&a[i]);
+printf("\n Enter item to be searched:");
+scanf("%d",&item);
+binary_search();
+//getch()
+}
+void binary_search()
+{
+
+    beg=0;end=n-1;
+    mid=(beg+end)/2;
+    while ((beg <=end )&&(a[mid]!=item))
+    {
+
+        if (item<a[mid])
+            end=mid-1;
+        else
+            beg=mid+1;
+        mid =(beg+end)/2;
+    }
+    if(a[mid]==item)
+        printf("\n\nITEM found at location %d",mid+1);
+    else
+        printf("\n\nITEM doesn't exist");
+    }
+
+
+
+
